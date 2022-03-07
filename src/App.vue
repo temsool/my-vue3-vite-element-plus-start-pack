358ad0799a59e35cfb40bd03d00c89335e8597a8
EMIN <amindiary@gmail.com>
2022-03-07 17:09:28 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template> 
+  <el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <span>Card name</span>
+        <el-button class="button" type="text">Operation button</el-button>
+      </div>
+    </template>
+    <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+  </el-card>
+</template>
+
+<style>
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card {
+  width: 480px;
+}
+</style>
